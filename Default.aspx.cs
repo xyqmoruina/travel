a1ecb7a4d410437a9eb672a8e5b7026bd13a776b
xyqmoruina <403095587@qq.com>
2013-11-14 21:29:20 +0800

    原型开发
    实现基本的框架，为用户展示基本的功能

---
+++ Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+         this.SqlDataSource1.SelectCommand = "SELECT name,description from router where name ='"+TextBox1.ToString()+"'";
+         this.SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+         GridView1.DataSourceID = "SqlDataSource1";
+         GridView1.DataBind();
+    }
+    protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+
+    }
+    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        switch (e.Row.RowType)
+        {
+            case DataControlRowType.DataRow:
+                //放置鼠标变色
+                e.Row.Attributes.Add("onmouseover", "c=this.style.backgroundColor;this.style.backgroundColor='#999999'");
+                e.Row.Attributes.Add("onmouseout", "this.style.backgroundColor=c");
+                e.Row.Attributes.Add("style", "cursor:pointer");
+                #region
+                PostBackOptions myPostBackOptions = new PostBackOptions(this);
+                myPostBackOptions.AutoPostBack = false;
+                myPostBackOptions.PerformValidation = false;
+                myPostBackOptions.RequiresJavaScriptProtocol = true; //加入javascript:头
+                String evt = Page.ClientScript.GetPostBackClientHyperlink(sender as GridView, "Select$" + e.Row.RowIndex.ToString());
+                e.Row.Attributes.Add("onclick", evt);
+                #endregion
+                break;
+        }
+    }
+}
---
+++ home.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+public partial class home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        /*string TempSql = "";
+        string ConStr = WebConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlConnection con = new SqlConnection(ConStr);
+        SqlCommand cmd = new SqlCommand(TempSql, con);
+        using (con)
+        {
+            con.Open();
+            SqlDataReader dr1 = cmd.ExecuteReader();
+            while (dr1.Read())
+            {
+                if (dr1[0].ToString() != "0")
+                {
+                    newOrder.Visible = true;
+                }
+                else
+                {
+                    newOrder.Visible = false;
+                }
+            }
+            dr1.Close();
+            con.Close();
+        }*/
+        Label1.Text = TextBox1.ToString();
+        this.SqlDataSource1.SelectCommand = "SELECT name,description from router where name like '%"+TextBox1.ToString()+"%'";
+        this.SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+        GridView1.DataSourceID = "SqlDataSource1";
+        GridView1.DataBind();
+    }
+}
